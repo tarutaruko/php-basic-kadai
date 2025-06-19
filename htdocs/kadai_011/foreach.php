94ef899a0cf78c1b1eb683db29c67c4c40f6b929
tarutaruko <tarutaruko39@gmail.com>
2025-06-19 22:40:48 +0900

    課題011

---
+++ htdocs/kadai_011/foreach.php
@@ -0,0 +1,6 @@
+      <?php
+        $items = ['name'=>'玉ねぎ','price'=>200,'area'=>'北海道'];
+        foreach ($items as $key => $value) {
+          echo "{$key}:{$value}".'<br>';
+        }
+      ?>
